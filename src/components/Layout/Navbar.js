99ce78cd5ed769d8f3895a634046d0571d9a841b
Tomas Vorel <tomvorel13@gmail.com>
2018-12-04 10:44:31 +0100

    Styled Components
    Started using styled components for my styles, working on the Navbar

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,13 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
 const Navbar = ({ title }) => {
   return (
-    <nav className="navbar">
-      <a href="#!" className="brand">
-        {title}
-      </a>
-    </nav>
+    <NavbarWrap>
+      <NavHeading>{title}</NavHeading>
+    </NavbarWrap>
   );
 };
+
+const NavbarWrap = styled.nav`
+  background-color: black;
+  font-family: "Roboto", sans-serif;
+  padding: 0.4rem 0;
+`;
+
+const NavHeading = styled.h1`
+  font-size: 2rem;
+  font-weight: 400;
+  margin: 0;
+  color: white;
+`;
 
 export default Navbar;
