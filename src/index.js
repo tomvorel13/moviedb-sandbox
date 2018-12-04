d648bdaf7fbb17a89062a036877496017b7cf7eb
Tomas Vorel <tomvorel13@gmail.com>
2018-12-04 08:55:39 +0100

    Bootstrap
    Adding bootstrap for some simple styling

--- src/index.js
+++ src/index.js
@@ -11,20 +11,32 @@ class App extends Component {
   };
 
   async componentDidMount() {
-    const res = await axios.get(
-      "https://api.themoviedb.org/3/movie/top_rated?api_key=c4f8cf8abe8461ffab5ac32209887546&language=en-US&page=1"
-    );
+    try {
+      const res = await axios.get(
+        "https://api.themoviedb.org/3/movie/top_rated?api_key=c4f8cf8abe8461ffab5ac32209887546&language=en-US&page=1"
+      );
 
-    const { results } = res.data;
+      const { results } = res.data;
 
-    this.setState({
-      movies: results
-    });
+      this.setState({
+        movies: results
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   render() {
     const { movies } = this.state;
-    const list = movies.map(movie => <li>{movie.title}</li>);
+    const list = movies.map(movie => (
+      <div key={movie.id}>
+        <img
+          src={`http://image.tmdb.org/t/p/w185/${movie.poster_path}`}
+          alt="Movie poster"
+        />
+        <h2>{movie.title}</h2>
+      </div>
+    ));
 
     return (
       <div className="App">
