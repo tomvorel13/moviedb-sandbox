3c0b03e2eb4b5f03864dfd2ec345b908c159d88a
Tomas Vorel <tomvorel13@gmail.com>
2018-12-10 11:59:26 +0100

    FontAwesome
    Added FontAwesome lib to add some icons for Movie cards

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import ReadMoreAndLess from "react-read-more-less";
 
 // Styles
 import styled from "styled-components";
 import { black } from "../styles/colors";
 import { Roboto } from "../styles/fonts";
 
-const MovieCard = ({ movie }) => {
-  return (
-    <Card>
-      <ImageWrap>
-        <Poster
-          src={`http://image.tmdb.org/t/p/w185/${movie.poster_path}`}
-          alt="Movie poster"
-        />
-      </ImageWrap>
-      <CardContentWrap>
-        <MovieTitle>{movie.title}</MovieTitle>
-        <MovieOverview>{movie.overview}</MovieOverview>
-      </CardContentWrap>
-    </Card>
-  );
-};
+// Components
+import Star from "../components/icons/Star";
+
+class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <Card>
+        <ImageWrap>
+          <Poster
+            src={`http://image.tmdb.org/t/p/w185/${movie.poster_path}`}
+            alt="Movie poster"
+          />
+        </ImageWrap>
+        <CardContentWrap>
+          <MovieTitle>{movie.title}</MovieTitle>
+          <MovieOverview>
+            <ReadMoreAndLess
+              ref={this.ReadMore}
+              className="read-more-content"
+              charLimit={150}
+              readMoreText="Read more"
+              readLessText="Read less"
+            >
+              {movie.overview}
+            </ReadMoreAndLess>
+          </MovieOverview>
+          <MovieRatingWrap>
+            <Star size="30px" /> <AvgRating>{movie.vote_average}</AvgRating>
+          </MovieRatingWrap>
+        </CardContentWrap>
+      </Card>
+    );
+  }
+}
 
 // Styles
 const Card = styled.div`
@@ -48,7 +68,17 @@ const CardContentWrap = styled.div`
 const MovieOverview = styled.p`
   margin: 0;
   font-family: ${Roboto};
+  color: ${black};
   font-weight: 300;
+  margin-bottom: 1rem;
+`;
+
+const MovieRatingWrap = styled.div``;
+
+const AvgRating = styled.span`
+  font-size: 1.6rem;
+  font-family: ${Roboto};
+  color: ${black};
 `;
 
 export default MovieCard;
---
+++ src/components/icons/Star.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Star = ({ size }) => {
+  return <i style={{ "font-size": size }} class="far fa-star" />;
+};
+
+export default Star;
